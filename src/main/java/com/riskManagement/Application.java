d374331397c25545edea1fc214ae4a425c8ec76c
samson.palkar <samson.palkar@gmail.com>
2017-02-13 13:53:51 -0600

    Master: Initial Commit

---
+++ src/main/java/com/riskManagement/Application.java
@@ -0,0 +1,35 @@
+package com.riskManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by samson.palkar on 2/13/2017.
+ */
+@SpringBootApplication
+@Controller
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Main end point for handling requests.
+     */
+    @RequestMapping("/")
+    @ResponseBody
+    public String mainEndPoint() throws Exception{
+        return "Home Page";
+    }
+}
