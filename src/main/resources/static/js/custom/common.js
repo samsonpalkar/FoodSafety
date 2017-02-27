b4ad72b81ed97e7b9bdd655e8f354b13101ba4c7
samson.palkar <samsonpalkar@gmail.com>
2017-02-27 11:50:11 -0600

    no message

--- src/main/resources/static/js/custom/common.js
+++ src/main/resources/static/js/custom/common.js
@@ -1,3 +1,12 @@
 /**
  * Created by samson.palkar on 2/15/2017.
  */
+function baseHref()
+{
+    var base = location.pathname.indexOf("/",1);
+    return location.protocol + "//" + location.host + location.pathname.substring(0,base) + "/";
+}
+function baseURL()
+{
+    baseHref();
+}
--- src/main/resources/static/js/custom/managerChecklist.js
+++ src/main/resources/static/js/custom/managerChecklist.js
@@ -1,3 +1,16 @@
 /**
  * Created by samson.palkar on 2/21/2017.
  */
+function loadForm(formName) {
+    $.ajax({
+        type: 'GET',
+        async: false,
+        dataType: 'text',
+        url: baseHref() + formName,
+        success: function(data, status){
+            console.log(data);
+            window.location.href = baseHref() + formName;
+            console.log(data);
+        }
+    });
+}
